ddb844c46c4b041489d736036bd1e347caf468f7
tanveen <tanveen.bharaj@utah.edu>
2017-09-09 20:19:17 -0600

    weather forecast

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_KEY="e8f167e3edf558a6ea68764e6864e8a0";
+const URL='http://samples.openweathermap.org/data/2.5/forecast?appid='+API_KEY;
+
+export const FETCH_WEATHER='FETCH_WEATHER';
+
+ export function fetchWeather(city) {
+     const url=`${URL}&q=${city},us`;
+     const request = axios.get(url); //request is promise
+     return {
+         type:FETCH_WEATHER,
+         payload:request
+     };
+ }
